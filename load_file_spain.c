16ce2bbb3995e1f859ffbdfa599018087211cfdb
Quim98 <69507121+Quim98@users.noreply.github.com>
2021-10-27 16:17:42 +0200

    Add files via upload

--- load_file_spain.c
+++ load_file_spain.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-typedef struct{
+typedef struct Node{
     unsigned long int id;
     char *name;
     double lat,lon;
@@ -34,7 +34,7 @@ unsigned long int binary_search ( node nodes_inf[], int size_nodes, int searched
 void put_successor ( node nodes_inf[], unsigned long int source, unsigned long int destination )
 {
     unsigned long int source_element;
-    int k;
+    unsigned short int k;
 
     // Search the element of the source node
     source_element = binary_search ( nodes_inf, 23895681, source );
@@ -57,76 +57,92 @@ void put_successor ( node nodes_inf[], unsigned long int source, unsigned long i
 
     // Add the successor
     *(nodes_inf[source_element].successors + (nodes_inf[source_element].nsucc - 1)) = destination;
-
+    return;
 }
 
 int main(int argc, char** argv)
 {
-    printf("prova1");
     FILE *myfile;
-    size_t size;
+    size_t size = 79857;
     char *buffer;
     int bytes_read;
-    unsigned long int row_num = 25353791; 
     unsigned long int node_count = 0;
-    unsigned long int node_num = 23895681;
     unsigned long int i;
     unsigned int j;
-    node node_inf[node_num];
+    node *node_inf;
+    if((node_inf = (node*)malloc(23895681*sizeof(node))) == NULL)
+    {
+        printf("Memory of nodes could not be reserved. Exiting the program. \n");
+        return 1;
+    }
     char *trash;
     char *first;
     char *last;
-    printf("prova2");
-
     
     myfile = fopen("spain.csv","r");
     if (myfile == NULL)
     {
         printf ("File was not found. \n");
-        return 0;
+        return 1;
     }
     else
     {
         printf ("Success at loading the file. \n");
     }
 
-    printf("prova3");
+    if((buffer = (char*) malloc (size*sizeof(char))) == NULL)
+    {
+        printf("Memory of buffer could not be reserved. Exiting the program. \n");
+        return 1;
+    }
 
     getline (&buffer, &size, myfile);
-    getline (&buffer, &size, myfile); /* Discard first 3 rows */
+    getline (&buffer, &size, myfile); // Discard first 3 rows
     getline (&buffer, &size, myfile);
 
-    for (i=3;i<row_num;i++)
+    printf("Reading line: '%s'\n",buffer); // Borrar!!!!!!!!!!!!!
+
+    printf("Prova 1 \n"); // Borrar!!!!!!!!!!!!!
+    
+    for (i=3;i<25353791;i++)
     {
         getline (&buffer, &size, myfile);
-        if ( strsep (&buffer,"|") == "node" )
+        if (i>2000) // Borrar!!!!!!!!!!!!!
+        { // Borrar!!!!!!!!!!!!!
+            printf("Reading line: '%s' \n",buffer); // Borrar!!!!!!!!!!!!!
+            printf("Line above in iteration: %d \n",i); // Borrar!!!!!!!!!!!!!
+        } // Borrar!!!!!!!!!!!!!
+        
+        first = strsep (&buffer,"|");
+        
+        if ( first == "node" )
         {
             node_inf[node_count].id = strtoul(strsep (&buffer,"|"),&trash,10);
             node_inf[node_count].name = strsep (&buffer,"|");
             for (j=0;j<7;j++)
             {
-                strsep (&buffer,"|"); // Lines that we don't want
+                trash = strsep(&buffer,"|"); // Lines that we don't want
             }
             node_inf[node_count].lat = atof(strsep (&buffer,"|"));
             node_inf[node_count].lon = atof(strsep (&buffer,"|"));
             node_inf[node_count].nsucc = 0;
-            node_inf[node_count].successors = (unsigned long int*)malloc(2*sizeof(unsigned long int));
+            ;
             // We assign a default vector size of 2 by reserving 2 times the memory needed for our data type
-            if ( node_inf[node_count].successors == NULL )
+            if ( (node_inf[node_count].successors = (unsigned long int*)malloc(2*sizeof(unsigned long int))) == NULL )
             {
-                printf("Memory could not be reserved. Exiting the program. \n");
-                return 0;
+                printf("Memory of successors could not be reserved. Exiting the program. \n");
+                return 1;
             }
             node_count += 1;
-            continue;
         }
-        if ( strsep (&buffer,"|") == "way" )
+        /*
+        else if ( first == "way" )
         {
             for (j=0;j<6;j++)
             {
-                strsep (&buffer,"|"); // Lines that we don't want
+                trash = strsep (&buffer,"|"); // Lines that we don't want
             }
-            if ( strsep (&buffer,"|") == "oneway" )
+            if ( (first = strsep (&buffer,"|")) == "oneway" )
             {
                 while ( (first = strsep (&buffer,"|")) != NULL )
                 {
@@ -144,7 +160,22 @@ int main(int argc, char** argv)
                 }
             }
         }
+        */
     }
     fclose(myfile);
+
+    printf("Prova 6 \n"); // Borrar!!!!!!!!!!!!!
+
+    /*
+    for (i=0;i<23895681;i=i+1000000) // See if we have saved the nodes
+    {
+        printf("The id of the node is: %d \n",node_inf[i].id);
+        printf("The latitude of the node is: %f \n",node_inf[i].lat);
+        printf("The longitude of the node is: %f \n",node_inf[i].lon);
+        printf("The number of successors of the node is: %d \n",node_inf[i].nsucc);
+    }
+    */
+
+
     return 0;
 }
