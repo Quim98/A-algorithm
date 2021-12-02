bbd919af3f123fc03387341f6b4ed1e4a9ccc222
Quim98 <69507121+Quim98@users.noreply.github.com>
2021-12-02 17:32:31 +0100

    Create main_Astar.c

---
+++ Functions/main_Astar.c
@@ -0,0 +1,12 @@
+    #include <stdio.h>
+    #include <string.h>
+    #include <stdlib.h>
+    #include <math.h>
+    #include "load_data.h"
+    #include "Astar.h"
+void main()
+{
+    load_data ();
+    Astar();
+    return;
+}
